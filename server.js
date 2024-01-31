25dd66213cb80191cdd85a5142d5612227754b68
Dan Janikowski <danjanikowski@gmail.com>
2024-01-31 14:20:12 -0500

    Added almost all the routing

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.all("*", (req, res, next) => {
 app.get("/api/:collection", async (req, res) => {
   try {
     const collection = database.collection(req.params["collection"]);
-    const characters = await collection.find().toArray();
-    res.json(characters);
+    const data = await collection.find().toArray();
+    res.json(data);
   } catch (error) {
     res.status(500).json({ error: error });
   }
@@ -35,90 +35,47 @@ app.get("/api/:collection", async (req, res) => {
 
 app.get("/api/:collection/:id", async (req, res) => {
   try {
-    // console.log(req.params["collection"]);
-    // console.log(req.params["id"]);
     const collection = database.collection(req.params["collection"]);
-    const characters = await collection
-      .findOne({ id: req.params["id"] })
-      .toArray();
-    res.json(characters);
+    const item = await collection.findOne({ "id": parseInt(req.params["id"]) });
+    res.json(item);
   } catch (error) {
     res.status(500).json({ error: error });
   }
 });
 
+// So for this: example film 1 characters
+// So in films_characters make a query to get all characters from the given film id
+// Then follow that up by getting an array of all the characters from their ids
 app.get("/api/:collection/:id/:sub", async (req, res) => {
-  console.log(`Tier 3`);
-  console.log(req.params);
+  try {
+    switch(req.params["collection"]) {
+      case 'films':
+        let items = [];
+        if (req.params["sub"] === 'characters') {
+          const collection = database.collection('films_characters');
+          const filmToChars = await collection.find({ "film_id": parseInt(req.params["id"]) }).toArray();
+          const characters = database.collection('characters');
+          for (let filmToChar of filmToChars) {
+            let item = await characters.findOne({"id": filmToChar.character_id});
+            items.push(item);
+          }
+        }
+        else if (req.params["sub"] === 'planets') {
+          const collection = database.collection('films_planets');
+          const filmToPlanets = await collection.find({ "film_id": parseInt(req.params["id"]) }).toArray();
+          const planets = database.collection('planets');
+          for (let filmToPlanet of filmToPlanets) {
+            let item = await planets.findOne({"id": filmToPlanet.planet_id});
+            items.push(item);
+          }
+        }
+        res.json(items);
+        break;
+    }
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 });
-
-// app.get("/api/characters", async (req, res) => {
-//   try {
-//     const collection = database.collection("characters");
-//     const characters = await collection.find().toArray();
-//     res.json(characters);
-//   } catch (error) {
-//     res.status(500).json({ error: error });
-//   }
-// });
-
-// app.get("/api/planets", async (req, res) => {
-//   try {
-//     const collection = database.collection("planets");
-//     const planets = await collection.find().toArray();
-//     res.json(planets);
-//   } catch (error) {
-//     res.status(500).json({ error: error });
-//   }
-// });
-
-// //Serve all the people at GET /people
-// app.get("/api/people", async (req, res) => {
-//   // res.send(people);
-//   try {
-//     const client = await MongoClient.connect("mongodb://localhost:27017");
-//     const db = client.db("edp");
-//     const collection = db.collection("people");
-//     const people = await collection.find().toArray();
-//     client.close();
-//     res.json(people);
-//   } catch (error) {
-//     res.status(500).json({ error: error });
-//   }
-// });
-
-// app.get("/api/people/:id", async (req, res) => {
-//   const id = req.params.id;
-//   try {
-//     const client = await MongoClient.connect("mongodb://localhost:27017");
-//     const db = client.db("edp");
-//     const collection = db.collection("people");
-//     const person = await collection.findOne({ id: +id });
-//     client.close();
-//     if (person) {
-//       res.json(person);
-//     } else {
-//       res.status(404).json({ message: "Person not found" });
-//     }
-//   } catch (error) {
-//     res.status(500).json({ error: error });
-//   }
-// });
-
-// app.post("/api/people", async (req, res) => {
-//   const data = req.body;
-//   try {
-//     const client = await MongoClient.connect("mongodb://localhost:27017");
-//     const db = client.db("edp");
-//     const collection = db.collection("people");
-//     const result = await collection.insertOne(data);
-//     client.close();
-//     res.status(201).json({ _id: result.insertedId });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ error: error });
-//   }
-// });
 
 app.use("", express.static("public/dist/index.html"));
 app.use("/index.html", express.static("public/dist/index.html"));
