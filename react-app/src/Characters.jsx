5f3e4caaa4a242e863cc18b45e7883846ab7ffac
Jacob Boyce <jboyce313@gmail.com>
2024-01-31 14:21:15 -0500

    added click event

--- react-app/src/Characters.jsx
+++ react-app/src/Characters.jsx
@@ -1,10 +1,17 @@
+import { getCharacter } from "./getData";
+
 export function Characters({ characters }) {
+  console.log(characters);
   return (
     <>
       <h1>Star Wars Universe Lookup</h1>
       {characters.map((character) => {
         return (
-          <div className="box" key={character.name}>
+          <div
+            handleClick={getCharacter(character._id)}
+            className="box"
+            key={character.name}
+          >
             <h3>{character.name}</h3>
           </div>
         );
--- react-app/src/getData.js
+++ react-app/src/getData.js
@@ -4,6 +4,12 @@ export async function getCharacters() {
   return characters;
 }
 
+export async function getCharacter(id) {
+  const url = `/api/characters/${id}`;
+  let character = await fetch(url).then((res) => res.json());
+  return character;
+}
+
 export async function getPlanets() {
   const url = `/api/planets`;
   let planets = await fetch(url).then((res) => res.json());
