82c595f1437b29d9e4fd1e79dabdcf78c61f2b61
Jacob Boyce <jboyce313@gmail.com>
2024-01-31 15:49:19 -0500

    added get character and homeworld to character.jsx

--- react-app/src/Character.jsx
+++ react-app/src/Character.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { getCharacter } from "./getData";
+import { getCharacter, getCharacterHomeworld } from "./getData";
 
 export function Character() {
   const params = useParams();
@@ -8,9 +8,28 @@ export function Character() {
   console.log(characterID);
 
   let [character, setCharacter] = useState({});
+  let [homeworld, setHomeworld] = useState({});
+
   useEffect(() => {
-    getCharacter(characterID).then((character) => setCharacter(character));
+    getCharacter(characterID)
+      .then((character) => setCharacter(character))
+      .then(() => setHomeworld(getCharacterHomeworld(character.id)));
   }, []);
 
-  return <h1>{character.name}</h1>;
+  //   useEffect(() => {
+  //     getCharacterHomeworld(character.homeworld).then((homeworld) =>
+  //       setHomeworld(homeworld)
+  //     );
+  //   }, []);
+
+  return (
+    <>
+      <h1>{character.name}</h1>
+      <p>Height: {character.height} cm</p>
+      <p>Mass: {character.mass} kg</p>
+      <p>Born: {character.birth_year}</p>
+
+      <h2>Homeworld</h2>
+    </>
+  );
 }
--- react-app/src/getData.js
+++ react-app/src/getData.js
@@ -10,6 +10,12 @@ export async function getCharacter(id) {
   return character;
 }
 
+export async function getCharacterHomeworld(id) {
+  const url = `/api/characters/${id}/planet`;
+  let homeworld = await fetch(url).then((res) => res.json());
+  return homeworld;
+}
+
 export async function getPlanets() {
   const url = `/api/planets`;
   let planets = await fetch(url).then((res) => res.json());
